201c15db630c829ab3a13ee80e9c8fdd70c21a46
ninh <ninhnguyen375@gmail.com>
2021-06-05 16:39:51 +0700

    Deploy

--- src/configs/dev.js
+++ src/configs/dev.js
@@ -1,7 +1,7 @@
 export const configs = {
   TIMEOUT: 50000,
-  API: 'https://point-traning-system-online.herokuapp.com/api',
-  APIHost: 'https://point-traning-system-online.herokuapp.com',
+  API: 'http://127.0.0.1:5000/api',
+  APIHost: 'http://127.0.0.1:5000',
   LogoURL:
     'http://fit.sgu.edu.vn/site/wp-content/uploads/2019/01/logover3-1.png',
   ServiceAPI: 'http://localhost:3001/api',
